d4eb0199c496a55ea8c25bffa5f447d70b8ac588
Zifeldev <zifel_end@mail.ru>
2024-09-30 20:56:57 +0500

    update

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const burger = document.querySelector("#button");
+const menu = document.querySelector('#menu');
+
+burger.addEventListener('click', () => {
+    menu.classList.toggle('disp')
+});
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
